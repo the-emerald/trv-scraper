94ce9818b78d1f76998887c616ea2dc6b26d9227
Emerald <itsemerald@protonmail.com>
2023-01-14 19:59:58 +0000

    fix: range not deserialized

--- api/src/fighter.rs
+++ api/src/fighter.rs
@@ -1,14 +1,14 @@
 use erc_nft_metadata::Metadata;
 use ethers_core::types::Address;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct FighterResponse {
     pub attributes: Attributes,
     pub statistic: Statistics,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Attributes {
     pub id: u64,
     pub champion_type: String,
@@ -16,14 +16,14 @@ pub struct Attributes {
     pub attributes: Metadata,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Statistics {
     pub wisdom: Wisdom,
     pub elo: u64,
     pub owner_address: Address,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Wisdom {
     pub point: u64,
     pub strength: Stat,
@@ -32,9 +32,31 @@ pub struct Wisdom {
     pub omega: Stat,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub struct Stat {
     pub current_range: u64,
+    pub from: u64,
+    pub to: u64,
+}
+
+impl<'de> Deserialize<'de> for Stat {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[derive(Clone, Debug, Deserialize)]
+        struct Helper {
+            current_range: u64,
+            range: [u64; 2],
+        }
+
+        let helper: Helper = Deserialize::deserialize(deserializer)?;
+        Ok(Self {
+            current_range: helper.current_range,
+            from: helper.range[0],
+            to: helper.range[1],
+        })
+    }
 }
 
 #[cfg(test)]
