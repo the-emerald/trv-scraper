6a1a79a7f9728fe758d8358bb5783ad740ab5f45
Emerald <itsemerald@protonmail.com>
2023-01-15 02:34:32 +0000

    fix: ctrl-c should kill service

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use anyhow::Result;
 use sea_orm::Database;
 use std::env;
 use task::fighter::ChampionTask;
+use tracing::warn;
 
 pub mod task;
 
@@ -17,45 +18,13 @@ async fn main() -> Result<()> {
 
     let client = reqwest::Client::new();
 
-    let join = tokio::task::spawn(ChampionTask::new(client, database, alchemy_api_key).run());
+    let _ = tokio::task::spawn(ChampionTask::new(client, database, alchemy_api_key).run());
 
-    let _ = join.await?;
-
-    // let futures = (0..10)
-    //     .map(|x| get_champion(&client, x))
-    //     .collect();
-
-    // let res = futures::future::join_all((0..100).map(|i| {
-    //     let client = &client;
-    //     async move {
-    //         let resp = get_champion(client, i).await?;
-    //         println!("completed {i}");
-    //         Ok::<FighterResponse, reqwest::Error>(resp)
-    //     }
-    // }))
-    // .await;
-
-    // let futs = (0..1000).map(|i| {
-    //     let client = &client;
-    //     async move {
-    //         let resp = get_champion(client, i).await?;
-    //         println!("completed {i}");
-    //         Ok::<FighterResponse, reqwest::Error>(resp)
-    //     }
-    // });
-    // // let mut futs = vec![];
-
-    // let res = futures::future::join_all(futs).await;
-
-    // for (i, item) in res.into_iter().enumerate() {
-    //     let item = item.unwrap();
-    //     println!("{} is: {}", i, item.attributes.champion_type);
-    // }
-
-    Ok(())
-
-    // for i in 0..100 {
-    //     let chamption = get_champion(&client, i).await.unwrap();
-    //     println!("{} is: {}", i, chamption.attributes.champion_type);
-    // }
+    match tokio::signal::ctrl_c().await {
+        Ok(()) => Ok(()),
+        Err(e) => {
+            warn!(e = ?e, "unable to listen to shutdown signal");
+            Ok(())
+        }
+    }
 }
